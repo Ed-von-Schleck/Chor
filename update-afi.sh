51d538c2f622f372831371068e08c1854cb53361
Andreas Fiebig <andreaspunktfiebig@googlemail.com>
2013-07-10 22:25:23 +0200

    added new song(s)

--- update-afi.sh
+++ update-afi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git add .
 git commit -a -m "added new song(s)"
+git pull --no-edit christian gh-pages
 python generate.py
 git commit -a -m "updated index"
 git push christian master:gh-pages
