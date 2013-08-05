c7d3ce5ec504290bd3bf350f3b73f47caac670a9
Christian Schramm <christian.h.m.schramm@gmail.com>
2013-08-05 17:37:03 +0200

    added new song(s)

--- update-christian.sh
+++ update-christian.sh
@@ -2,7 +2,7 @@
 git add .
 git commit -a -m "added new song(s)"
 git pull --no-edit afi master
-git pull --no-edit upstream gh-pages
+git pull --no-edit origin gh-pages
 python generate.py
 git commit -a -m "updated index"
 git push origin gh-pages
