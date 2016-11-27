3d770d9ed25ce862ccaf0f45b9da388cb0e66c4e
Sven Möllers <sven.moellers@gmail.com>
2016-11-27 18:24:43 +0100

    remove onload basic playback example

--- generate.py
+++ generate.py
@@ -267,26 +267,6 @@ def main():
     <script src="midijs/js/util/dom_request_script.js" type="text/javascript"></script>
   </head>
   <body>
-<script type="text/javascript">
-window.onload = function () {
-	MIDI.loadPlugin({
-		soundfontUrl: "midijs/soundfont/",
-		instrument: "acoustic_grand_piano",
-		onprogress: function(state, progress) {
-			console.log(state, progress);
-		},
-		onsuccess: function() {
-			var delay = 0; // play one note every quarter second
-			var note = 50; // the MIDI note
-			var velocity = 127; // how hard the note hits
-			// play the note
-			MIDI.setVolume(0, 127);
-			MIDI.noteOn(0, note, velocity, delay);
-			MIDI.noteOff(0, note, delay + 0.75);
-		}
-	});
-};
-</script>
     <header>
     <h1>Physikerchor</h1>
 
